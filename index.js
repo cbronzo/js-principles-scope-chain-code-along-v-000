107cfc1b8d0d0985c4792003f54bd9006eea1413
Catherine Bronzo <cbronzo@mac.com>
2019-03-16 05:38:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 /* Your code goes here */
+
+customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'John'
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'bob';
+ }
+ 
+ function attemptTwoFavoriteCustomers(){
+   
+   
+   favoriteCustomer = 
+ }
